dd997517635db00bac290c5b3c9173e969219fba
xrist <xrist@192.168.1.13>
2021-04-27 19:03:03 +0300

    ok 2o eirhnh geia soy prepei na deis pente eji ok sto println

--- testaki/src/testaki/hell.java
+++ testaki/src/testaki/hell.java
@@ -5,7 +5,7 @@ public class hell {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("helloQ");
+		System.out.println("helloQ o k ok o k ok ok ok ok o");
 	}
 
 }
